b2658ff2fe9444a612ff527e0d47cfbb9bbfbae1
Nina <105605826+ananasuu@users.noreply.github.com>
2025-05-19 10:49:33 +0000

    Adjust Links & content

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const bbLinks = [
             {
                 title: "Laudna - Critical Role",
-                url: "https://example.com/link1",
+                url: "https://drive.google.com/file/d/1p_WrvoevjBnTGTBLTPl9-cajugDPxGX6/view?usp=drive_link",
                 description: "Build book for Laudna from Critical Role, competed and placed 3rd at 2024 Dokomi.",
-                imageUrl: "https://example.com/image1.jpg"
+                imageUrl: "https://scontent-dus1-1.cdninstagram.com/v/t51.29350-15/473673723_591115356866377_7776818308298947936_n.heic?stp=dst-jpg_e35_p750x750_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTUyeDE0NDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-dus1-1.cdninstagram.com&_nc_cat=101&_nc_oc=Q6cZ2QGU9uvxZpYG0o3KVXq4wKnSLftCtwA2w7Gnt5p7mIsffbe1sqfqSGwqRa0COJrbTBY&_nc_ohc=ninz9eCW59IQ7kNvwGLEj94&_nc_gid=_HUDKUqfvAgDqt0jnDv1WQ&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AfKdnEiXtx7yiN_nGm9AafTe2BHyAis4-hur3evkEJT-_w&oe=6830E483&_nc_sid=d885a2"
             },
             {
                 title: "Marcille Donato - Delicious in Dungeon",
-                url: "https://example.com/link2",
+                url: "https://drive.google.com/file/d/1fD7-P-gT0MA40vvP3u0L8dax4ZUXiO20/view?usp=drive_link",
                 description: "Build book for Marcille Donato from Delicious in Dungeon, competed and placed 1st at 2025 S.a.m.t Con.",
-                imageUrl: "https://example.com/image2.jpg"
+                imageUrl: ""
             }
         ];
 
