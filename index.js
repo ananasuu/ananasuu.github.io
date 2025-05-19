3eae07a13a7feb3d6706cf3b42eda06ff079f7ae
Nina <105605826+ananasuu@users.noreply.github.com>
2025-05-19 11:07:06 +0000

    Adjust link-content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bbLinks = [
                 title: "Laudna - Critical Role",
                 url: "https://drive.google.com/file/d/1p_WrvoevjBnTGTBLTPl9-cajugDPxGX6/view?usp=drive_link",
                 description: "Build book for Laudna from Critical Role, competed and placed 3rd at 2024 Dokomi.",
-                imageUrl: "https://scontent-dus1-1.cdninstagram.com/v/t51.29350-15/473673723_591115356866377_7776818308298947936_n.heic?stp=dst-jpg_e35_p750x750_sh0.08_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTUyeDE0NDAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-dus1-1.cdninstagram.com&_nc_cat=101&_nc_oc=Q6cZ2QGU9uvxZpYG0o3KVXq4wKnSLftCtwA2w7Gnt5p7mIsffbe1sqfqSGwqRa0COJrbTBY&_nc_ohc=ninz9eCW59IQ7kNvwGLEj94&_nc_gid=_HUDKUqfvAgDqt0jnDv1WQ&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AfKdnEiXtx7yiN_nGm9AafTe2BHyAis4-hur3evkEJT-_w&oe=6830E483&_nc_sid=d885a2"
+                imageUrl: "assets/img/card_img/Laudna_NF.jpg"
             },
             {
                 title: "Marcille Donato - Delicious in Dungeon",
@@ -15,20 +15,20 @@ const bbLinks = [
 
 const helpfulLinks = [
             {
-                title: "Tutorial - How to tech",
-                url: "https://example.com/link1",
-                description: "Light up your cosplay with this tutorial on how to tech your cosplay.",
-                imageUrl: "https://example.com/image1.jpg"
+                title: "Tutorial - Sewing a Corset",
+                url: "https://www.youtube.com/watch?v=alQGJQzRRFo&ab_channel=SarahSpaceman",
+                description: "Easily pattern and sew a corset.",
+                imageUrl: "http://img.youtube.com/vi/VIDEO-ID/hqdefault.jpg"
             }
         ];
 
 const toolLinks = [
             {
                 title: "Nifegun",
-                use: "Measuring tool for cosplay",
-                url: "https://example.com/link1",
-                description: "Ever wondered how to actually have the right skale for your props?",
-                imageUrl: "https://example.com/image1.jpg"
+                use: "Scaling Tool",
+                url: "https://nifegun.com/scale_tool/scale_tool.html",
+                description: "Ever wondered how to actually have the right scale for your props? Use this!",
+                imageUrl: "https://nifegun.com/scale_tool/images/atlas_gauntlet.png"
             }
         ];
 
