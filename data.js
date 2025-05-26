3859bfad6ef6f100648a8a932bc4c4af2adb3634
Nina <105605826+ananasuu@users.noreply.github.com>
2025-05-26 11:49:30 +0000

    Füge hilfreiche Links, Werkzeuge und Cosplays hinzu: Datenstruktur für Tutorials und Materialien erstellt

---
+++ data.js
@@ -0,0 +1,113 @@
+export const helpfulLinks = [
+    {
+        title: "Sewing a Corset",
+        use: "Tutorial",
+        url: "https://www.youtube.com/watch?v=alQGJQzRRFo&ab_channel=SarahSpaceman",
+        description: "Easily pattern and sew a corset.",
+        imageUrl: "http://img.youtube.com/vi/alQGJQzRRFo/hqdefault.jpg"
+    }, {
+        title: "Materials & Tools",
+        use: "Collection",
+        url: "https://www.belloflostsouls.net/2022/08/tools-of-the-trade-a-guide-to-cosplay-materials.html",
+        description: "Cosplay materials and tools overview.",
+        imageUrl: "https://www.belloflostsouls.net/wp-content/themes/bols-2022/img/logo-2021a.svg"
+    },
+    {
+        title: "Ears on a Budget",
+        use: "Tutorial",
+        url: "https://www.tiktok.com/@ananasu.exe/video/7289790044393442593",
+        description: "How to make animal ears on a budget.",
+        imageUrl: ""
+    },
+    {
+        title: "Maidheadband",
+        use: "Tutorial",
+
+        url: "https://www.tiktok.com/@ananasu.exe/video/7246414817773260059",
+        description: "How to make a simple maid headband.",
+        imageUrl: ""
+    },
+    {
+        title: "How to make horns",
+        use: "Tutorial",
+
+        url: "https://www.tiktok.com/@ananasu.exe/video/7247592635219627290",
+        description: "How to make lightweight horns without a 3d-Printer.",
+        imageUrl: ""
+    },
+];
+
+export const toolLinks = [
+    {
+        title: "Nifegun",
+        use: "Scaling Tool",
+        url: "https://nifegun.com/scale_tool/scale_tool.html",
+        description: "Ever wondered how to actually have the right scale for your props? Use this!",
+        imageUrl: "https://nifegun.com/scale_tool/images/atlas_gauntlet.png"
+    },
+    {
+        title: "Cosplayshop.be",
+        use: "Cosplay Materials, EU",
+        url: "https://cosplayshop.be/de/",
+        description: "Ordering materials in the EU can be a pain, but this shop has nice stuff!",
+        imageUrl: "https://cosplayshop.be/wp-content/themes/cosplayshop/img/logo.svg"
+    },
+    {
+        title: "Kamui Cosplay",
+        use: "Tutorials & Patterns",
+        url: "https://www.kamuicosplay.com/",
+        description: "Kamui Cosplay is a well-known cosplayer and prop maker. She has a lot of tutorials and patterns for sale.",
+        imageUrl: "https://www.kamuicosplay.com/wp-content/uploads/2020/09/Kamui_Logo.png"
+    }
+];
+
+export const cosPortfolio = [
+    {
+        title: "Marcille Donato",
+        source: "Delicious in Dungeon",
+        imageUrl: "assets/img/card_img/Marcille_Donato_NF.jpg",
+        genres: "Silly, Powerful & Hungry!",
+        description: "Marcille Donato is a character from a story about adventurers cooking and eating monsters. I made this cosplay for the 2025 S.a.m.t Con cosplay contest and placed 1st.",
+        buildbookUrl: "https://drive.google.com/file/d/1fD7-P-gT0MA40vvP3u0L8dax4ZUXiO20/view?usp=drive_link"
+    },
+    {
+        title: "Frieren",
+        source: "Frieren: Beyond Journey's End",
+        imageUrl: "assets/img/card_img/Frieren_NF.jpg",
+        genres: "Adventurous, Wise & Kind!",
+        description: "Frieren is a character from a story about an elf mage who travels to understand humanity. I competed with this cosplay at the 2024 Bijutsu & won best in craftsmanship.",
+        buildbookUrl: ""
+    },
+    {
+        title: "Laudna",
+        source: "Critical Role",
+        imageUrl: "assets/img/card_img/Laudna_NF.jpg",
+        genres: "Dark, Spooky & Fun!",
+        description: "Laudna is a character from Critical Role, a D&D show. I made this cosplay for the 2024 Dokomi cosplay contest and placed 3rd.",
+        buildbookUrl: "https://drive.google.com/file/d/1p_WrvoevjBnTGTBLTPl9-cajugDPxGX6/view?usp=drive_link"
+    },
+    {
+        title: "Tohru",
+        source: "Miss Kobayashi's Dragon Maid",
+        imageUrl: "assets/img/card_img/Tohru_NF.jpg",
+        genres: "Cute, Outgoing & Funny!",
+        description: "Tohru is a dragon mastering her life with her savior Miss Kobayashi. This slice of life anime is a pure comfort show.",
+        buildbookUrl: ""
+    },
+    {
+        title: "Haru",
+        source: "Beastars",
+        imageUrl: "assets/img/card_img/Haru_NF.jpg",
+        genres: "Dramatic, Inspiring & Tough!",
+        description: "Haru is living in a world where carnivores and herbivores are at odds. She is a strong character who fights for her beliefs.",
+        buildbookUrl: ""
+    },
+    {
+        title: "Fearne Calloway",
+        source: "Critical Role",
+        imageUrl: "assets/img/card_img/Fearne_NF.jpg",
+        genres: "Silly, Mischievous & Fun!",
+        description: "Fearne is a character from Critical Role, a D&D show. Her fey nature healed something in me & she enchants enyone who meets her.",
+        buildbookUrl: ""
+    }
+]
